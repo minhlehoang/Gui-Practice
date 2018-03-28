49caa994a2d9b11ca6f75f7ca7e4a21aa5c0c694
Minh H Le <minhhoangle1104@gmail.com>
2018-03-28 10:56:09 -0600

    6th commit
    create date object

--- UserGUI.java
+++ UserGUI.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.io.FileNotFoundException;
+import java.time.LocalDate;
 import java.util.Scanner;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
@@ -12,8 +13,11 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -37,6 +41,10 @@ public class UserGUI extends JFrame implements ActionListener{
 	private JTextField state_field;
 	private JLabel zipcode;
 	private JTextField zipcode_field;
+	private JLabel phonenumber;
+	private JTextField phonenumber_field;
+	private JLabel email;
+	private JTextField email_field;
 	private JRadioButton student_radio;
 	private JRadioButton faculty_radio;
 	private ButtonGroup student_or_faculty_button_group;
@@ -54,6 +62,7 @@ public class UserGUI extends JFrame implements ActionListener{
 	private JPanel status_or_rank_panel;
 	private JPanel textarea_panel;
 	private JPanel some_buttons_panel;
+	String student_status_text;
 	// constructor
 	UserGUI(int nPersons){
 		// create person array of size nPersons
@@ -78,19 +87,23 @@ public class UserGUI extends JFrame implements ActionListener{
 	private void initComponenet(){
 		// initialize all user interface components
 		firstname = new JLabel("First Name");
-		firstname_field = new JTextField("What is your firstname ?");
+		firstname_field = new JTextField();
 		lastname = new JLabel("Last Name");
-		lastname_field = new JTextField("What is your lastname ?");
-		street = new JLabel("Street");
-		street_field = new JTextField("Street name ?");
+		lastname_field = new JTextField();
+		street = new JLabel("Street Name");
+		street_field = new JTextField();
 		housenumber = new JLabel("House Number");
-		housenumber_field = new JTextField("What is your house number ?");
+		housenumber_field = new JTextField();
 		city = new JLabel("City");
-		city_field = new JTextField("City ?");
+		city_field = new JTextField();
 		state = new JLabel("State");
-		state_field = new JTextField("State ?");
+		state_field = new JTextField();
 		zipcode = new JLabel("Zipcode");
-		zipcode_field = new JTextField("Zipcode ?");
+		zipcode_field = new JTextField();
+		phonenumber = new JLabel("Phone Number");
+		phonenumber_field = new JTextField();
+		email = new JLabel("Email");
+		email_field = new JTextField();
 		student_radio = new JRadioButton("Student");
 		student_radio.setSelected(true);
 		faculty_radio = new JRadioButton("Faculty");
@@ -109,7 +122,7 @@ public class UserGUI extends JFrame implements ActionListener{
 		sort_student_faculty = new JButton("Sort Student & Faculty");
 		display_student_faculty = new JButton("Display Student & Faculty");
 		main_panel = new JPanel(new GridLayout(5, 1));
-		info_panel = new JPanel(new GridLayout(7, 2));
+		info_panel = new JPanel(new GridLayout(5, 4));
 		student_or_faculty_panel = new JPanel(new GridLayout(2, 1));
 		status_or_rank_panel = new JPanel(new GridLayout(1, 1));
 		textarea_panel = new JPanel(new FlowLayout());
@@ -122,16 +135,20 @@ public class UserGUI extends JFrame implements ActionListener{
 		info_panel.add(firstname_field);
 		info_panel.add(lastname);
 		info_panel.add(lastname_field);
-		info_panel.add(street);
-		info_panel.add(street_field);
 		info_panel.add(housenumber);
 		info_panel.add(housenumber_field);
+		info_panel.add(street);
+		info_panel.add(street_field);
 		info_panel.add(city);
 		info_panel.add(city_field);
 		info_panel.add(state);
 		info_panel.add(state_field);
 		info_panel.add(zipcode);
 		info_panel.add(zipcode_field);
+		info_panel.add(phonenumber);
+		info_panel.add(phonenumber_field);
+		info_panel.add(email);
+		info_panel.add(email_field);
 		student_or_faculty_panel.add(student_radio);
 		student_or_faculty_panel.add(faculty_radio);
 		status_or_rank_panel.add(student_combobox);
@@ -146,7 +163,6 @@ public class UserGUI extends JFrame implements ActionListener{
 		main_panel.add(some_buttons_panel);
 		add(main_panel);
 		}
-
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -182,7 +198,14 @@ public class UserGUI extends JFrame implements ActionListener{
 	
 	private void addButtonClicked(){
 		//Method to implement add button action
-		JOptionPane.showMessageDialog(null, "add button clicked");
+		MyDate day_object;
+		LocalDate today = LocalDate.now();
+		int month = today.getMonthValue();
+		int year = today.getYear();
+		int day = today.getDayOfMonth();
+		day_object = new MyDate(year, month, day);
+		
+		JOptionPane.showMessageDialog(null, "add button clicked " + day);
 		String first_name_text = firstname_field.getText();
 		String last_name_text = lastname_field.getText();
 		String street_text = street_field.getText();
@@ -190,15 +213,35 @@ public class UserGUI extends JFrame implements ActionListener{
 		String city_text = city_field.getText();
 		String state_text = state_field.getText();
 		int zipcode_text = Integer.parseInt(zipcode_field.getText());
+		Address address =  new Address(street_text, housenumber_text, city_text, state_text, zipcode_text);
+		String phonenumber_text = phonenumber_field.getText();
+		String email_text = email_field.getText();
 		if(student_radio.isSelected()) {
-			String student_status_text =  student_combobox.getSelectedItem().toString();
+			student_status_text =  student_combobox.getSelectedItem().toString();			
 			JOptionPane.showMessageDialog(null, student_status_text);
+			if(student_combobox.getSelectedItem().toString() != "Please select one of these options") {
+				JOptionPane.showMessageDialog(null, student_status_text);
+				Person person = new Student(first_name_text, last_name_text, address, phonenumber_text, email_text, student_status_text);
+				JOptionPane.showConfirmDialog(null, person.numberOfPersons);
+				textarea.setText("the person has been added");
+				
+				//personArray[i] = person;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "do it again");
+			}
 		}
 		else if(faculty_radio.isSelected()) {
 			String faculty_rank_text = faculty_combobox.getSelectedItem().toString();
 			JOptionPane.showMessageDialog(null, faculty_rank_text);
+			if(faculty_combobox.getSelectedItem().toString() != "Please select one of these options") {
+				Person person = new Faculty(first_name_text, last_name_text, address, phonenumber_text, email_text, faculty_rank_text, day_object);
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "do it again");
+			}
 		}
-		Address address =  new Address(street_text, housenumber_text, city_text, state_text, zipcode_text);
+		
 		
 		//JOptionPane.showMessageDialog(null, first_name_text + last_name_text + street_text + housenumber_text + city_text + state_text + zipcode_text);
 	}
